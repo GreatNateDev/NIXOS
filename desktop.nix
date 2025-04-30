50d5a13f918d3dedc5451ad2ac5237bf91cd1359
GreatNateDev <Nate@pelmel.net>
2025-04-29 20:34:49 -0500

    colors

--- desktop.nix
+++ desktop.nix
@@ -1,5 +1,13 @@
 {config, pkgs, ...}:
 {
+  programs.dconf.enable = true;
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark"; 
+      gtk-theme = "Adwaita-dark";   
+    };
+  };
+  environment.sessionVariables.GTK_THEME = "Adwaita-dark";
   services.xserver = {
     enable = false;
     displayManager.gdm.enable = true;
